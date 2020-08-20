5534fbf04961c26e7ce9ae08a8768d6c6bfbe9f9
Daniel Slyman <github@skylayer.eu>
2020-08-20 11:11:20 +0200

    Launch

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Skylayer Blueprint Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Skylayer Blueprint
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_SKYLAYER_BLUEPRINT_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'skylayer-blueprint-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_SKYLAYER_BLUEPRINT_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/danielslyman/skylayer-blueprint/',
+	__FILE__,
+	'skylayer-blueprint'
+);
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('your-token-here');
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('stable-branch-name');
